074ea29a68cfb7ecb1be4466f625e215150cbc72
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-12-15 02:01:26 +0330

    lesson-6

---
+++ 02-js-basics/06-string methods/app.js
@@ -0,0 +1,8 @@
+let fullName = "kani badavi";
+
+// let result = fullName.lastIndexOf("a")
+// let result = fullName.slice(0 , 4)
+// let result = fullName.substr(4 , 10)
+// let result = fullName.replace("a" , "b")
+
+console.log(result);
