62e354b569240fd28447fabe32b9b71801363f1c
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-12-17 15:24:03 +0330

    added numbers

---
+++ 02-js-basics/07-numbers/app.js
@@ -0,0 +1,22 @@
+let radius = 10;
+const pi = 3.14;
+
+//math operators + - *  ** / %
+console.log(pi * radius);
+let result = pi * radius ** 2;
+console.log(result);
+
+//order of operations
+console.log(2 * (12 - 5) ** 3);
+
+let likes = 10;
+likes++;
+likes--;
+likes += 10;
+likes -= 5;
+likes *= 2;
+likes /= 5;
+
+//NaN
+console.log(5 / "hello");
+
