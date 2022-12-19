d91d56261523d97ac1f01a46c1b0b012aee7da6d
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-12-19 20:00:45 +0330

    new changes

---
+++ 03-control flow basics/03-while loops/app.js
@@ -0,0 +1,14 @@
+// let i = 0
+// while (i < 5) {
+//     console.log(i);
+//     i++
+// }
+
+
+const names = ["zahra", "sara", "kani"]
+let i = 0;
+
+while (i < names.length) {
+    console.log(names[i]);
+    i++
+}
---
+++ 03-control flow basics/04-do while loops/app.js
@@ -0,0 +1,7 @@
+const names = ["sara", "zahra"]
+let i = 0;
+do{
+    console.log(names[i]);
+    i++;
+}
+while (i < names.length);
