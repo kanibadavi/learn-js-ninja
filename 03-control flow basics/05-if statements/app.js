06b085f55272c477b9671aa4f37aee0281cb06ca
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-12-19 23:04:53 +0330

    if

---
+++ 03-control flow basics/05-if statements/app.js
@@ -0,0 +1,18 @@
+const age = 23;
+
+if (age > 20) {
+    console.log('your age is big enough');
+}
+
+
+const ninjas = ['sara', 'zahra', 'fateme'];
+
+if (ninjas.length > 2) {
+    console.log('there are too many ninjas');
+}
+
+const password = 'p@ssword';
+
+if (password.length = 8) {
+    console.log('valid password');
+}
