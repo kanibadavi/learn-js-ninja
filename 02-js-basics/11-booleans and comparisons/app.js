3840621bfd3b47a1899490e6fbd6d6cce70c03e2
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-12-19 18:28:09 +0330

    changes

---
+++ 02-js-basics/11-booleans and comparisons/app.js
@@ -0,0 +1,26 @@
+//methods can return booleans
+
+const myName = "kani";
+const names = ["kani", "sara", "zahra"]
+
+console.log(myName.includes("k"));
+console.log(names.includes("sara"));
+
+//comparison operators
+
+let age = 25;
+
+console.log(age == 25);
+console.log(age != 25);
+console.log(age < 34);
+
+let name = "kani";
+
+console.log(name = "kani");
+console.log(name = "Kani");
+console.log(name > "cani");
+console.log(name > "Kani");
+console.log(name > "Cani");
+
+
+
---
+++ 02-js-basics/12-loose vs strict/app.js
@@ -0,0 +1,15 @@
+const num = 23;
+
+//loose comparison(different types can be equal)
+
+console.log(num == 25);
+console.log(num == '25');
+console.log(num != 25);
+console.log(num != '25');
+
+//strict comparison(differrent types cannot be equal)
+
+console.log(num === 25);
+console.log(num === '25');
+console.log(num !== 25);
+console.log(num !== '25');
