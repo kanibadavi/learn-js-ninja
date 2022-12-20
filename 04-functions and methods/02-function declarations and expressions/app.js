21fa3065753d8ee6b24f5256bdfd9f7a76ef9302
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-12-20 14:14:19 +0330

    function

---
+++ 04-functions and methods/02-function declarations and expressions/app.js
@@ -0,0 +1,13 @@
+//function declaration
+function greet() {
+    console.log('first function');
+}
+
+greet()
+
+//function expresion
+const speak = function(){
+    console.log('second function');
+}
+
+speak()
