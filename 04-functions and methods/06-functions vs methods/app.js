835e42d9966914326f8bb665a99e13daeb9ce71d
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-12-22 11:14:48 +0330

    methods

---
+++ 04-functions and methods/06-functions vs methods/app.js
@@ -0,0 +1,11 @@
+//functions
+
+const greet =() => "hello";
+
+const result = greet();
+console.log(result);
+
+//methods
+const name = "kani";
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
